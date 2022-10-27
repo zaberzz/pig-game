d27ee506f0060c7d4025beb0d37887d82b6d8fc4
Saba <saba.sadegh67@gmail.com>
2022-10-27 22:58:10 +0330

    changing the current player

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 // Defining variables
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
-const curren0EL = document.querySelector("#current--0");
-const curren1EL = document.querySelector("#current--1");
+const current0EL = document.querySelector("#current--0");
+const current1EL = document.querySelector("#current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -15,15 +17,22 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling the dice
 btnRoll.addEventListener("click", function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
-  if (dice !== 0) {
+  if (dice !== 1) {
     currentScore += dice;
-    curren0EL.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle("player--active");
+    player1EL.classList.toggle("player--active");
   }
 });
