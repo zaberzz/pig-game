1fa40e50de7ec663d686bbf0be80b8aafa96a5c3
Saba <saba.sadegh67@gmail.com>
2022-10-29 14:36:01 +0330

    add functionality to reset btn and refactor code

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const player0EL = document.querySelector(".player--0");
 const player1EL = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
-const current0EL = document.querySelector("#current--0");
-const current1EL = document.querySelector("#current--1");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -17,10 +17,27 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  diceEl.classList.add("hidden");
+  player0EL.classList.remove("player--winner");
+  player1EL.classList.remove("player--winner");
+  player0EL.classList.add("player--active");
+  player1EL.classList.remove("player--active");
+};
+
+init();
 
 const playerChange = function () {
   document.querySelector(`#current--${activePlayer}`).textContent = 0;
@@ -65,3 +82,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", init);
